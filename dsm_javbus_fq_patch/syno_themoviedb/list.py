d6685a38d0aa01fe2040cf49223c6958ac5a0d40
Варвара <iscreamusa319@gmail.com>
2020-07-18 11:05:19 +0800

    update

---
+++ dsm_javbus_fq_patch/syno_themoviedb/list.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+#-*- coding:utf8 -*-
+# By sanyle 2018-05-03
+
+import requests
+import re,sys,json,os
+from bs4 import BeautifulSoup
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+def javlist(title):
+    url = "https://www.javbus.com/search/"+title+"&type=&parent=ce"
+    r = requests.get(url)
+    r = r.content.decode("utf-8")
+    soup = BeautifulSoup(r,"lxml")
+    list = soup.select("#waterfall .item")
+    json = {}
+    listnum = len(list)
+    total = listnum
+    data = []
+    for i in range(len(list)):
+        if i>3:
+            break
+        vmsg = {}
+        id = list[i].select("date")[0].text
+        title = list[i].select(".photo-frame >img")[0].get("title").strip()
+        vmsg['summary'] = title
+        title = id + " "+ title
+        poster = list[i].select(".photo-frame > img")[0].get("src")
+        # if poster.find("http")==-1:
+        #     poster="https:"+poster
+        vmsg['id'] = id
+        vmsg['url'] = list[i].select(".movie-box")[0].get("href").replace("https://www.javbus.com/","")
+        vmsg['sub_title'] = id
+        #是否含有字幕
+        if list[i].select(".item-tag")[0].select(".btn-warning"):
+            vmsg['caption']=1
+            title = "[字幕]"+title
+        else:
+            vmsg['caption']=0
+        vmsg['title'] = title
+        vmsg['subtype'] = 'movie'
+        vmsg['lang'] = 'jpn'
+        vmsg['poster'] = poster
+        data.append(vmsg)
+    #搜索无码资源
+    url = "https://www.javbus.com/uncensored/search/"+title+"&type=&parent=ce"
+    r = requests.get(url)
+    r = r.content.decode("utf-8")
+    soup = BeautifulSoup(r,"lxml")
+    list = soup.select("#waterfall .item")
+    json = {}
+    listnum = len(list)
+    json['total'] = total+listnum
+    for i in range(len(list)):
+        if i>2:
+            break
+        vmsg = {}
+        id = list[i].select("date")[0].text
+        title = list[i].select(".photo-frame >img")[0].get("title").strip()
+        vmsg['url'] = list[i].select(".movie-box")[0].get("href").replace("https://www.javbus.com/","")
+        vmsg['summary'] = title
+        title = id + " "+ title
+        poster = list[i].select(".photo-frame > img")[0].get("src")
+        # if poster.find("http")==-1:
+        #     poster="https:"+poster
+        vmsg['id'] = id
+        
+        vmsg['sub_title'] = id
+        #是否含有字幕
+        if list[i].select(".photo-info"):
+            if list[i].select(".photo-info")[0].select(".btn-warning"):
+                vmsg['caption']=1
+                title = "[??]"+title
+            else:
+                vmsg['caption']=0
+        vmsg['title'] = title
+        vmsg['subtype'] = 'movie'
+        vmsg['lang'] = 'jpn'
+        vmsg['type']=1
+        vmsg['poster'] = poster
+        data.append(vmsg)
+    json['data'] = data
+    return json
+
+def main(argv):
+    title = "".join(argv)
+    list =  javlist(title)
+    data = json.dumps(list, ensure_ascii=False)
+    print(data)
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ dsm_javbus_patch/syno_themoviedb/list.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+#-*- coding:utf8 -*-
+# By sanyle 2018-05-03
+
+import requests
+import re,sys,json,os
+from bs4 import BeautifulSoup
+
+reload(sys)
+#sys.setdefaultencoding('utf8')
+host="https://www.dmmsee.zone/"
+def javlist(title):
+    # 获取有码信息
+    url = host+ "search/"+title+"&type=&parent=ce"
+    r = requests.get(url)
+    r = r.content.decode("utf-8")
+    soup = BeautifulSoup(r,"lxml")
+    list = soup.select("#waterfall .item")
+    json = {}
+    listnum = len(list)
+    total = listnum
+    data = []
+    for i in range(len(list)):
+        if i>2:
+            break
+        vmsg = {}
+        id = list[i].select("date")[0].text
+        title = list[i].select(".photo-frame >img")[0].get("title").strip()
+        vmsg['summary'] = title
+        title = id + " "+ title
+        vmsg['url'] = list[i].select(".movie-box")[0].get("href").replace(host,"")
+        poster = list[i].select(".photo-frame > img")[0].get("src")
+        # if poster.find("http")==-1:
+        #     poster="https:"+poster
+        vmsg['id'] = id
+        
+        vmsg['sub_title'] = id
+        #检查是否存在字幕
+        if list[i].select(".item-tag"):
+            if list[i].select(".item-tag")[0].select(".btn-warning"):
+                vmsg['caption']=1
+                title = "[字幕]"+title
+            else:
+                vmsg['caption']=0
+        vmsg['title'] = title
+        vmsg['subtype'] = 'movie'
+        vmsg['lang'] = 'jpn'
+        vmsg['type'] = 0 
+        vmsg['poster'] = poster
+        data.append(vmsg)
+    #获取无码信息
+    url = host + "uncensored/search/"+title+"&type=&parent=ce"
+    r = requests.get(url)
+    r = r.content.decode("utf-8")
+    soup = BeautifulSoup(r,"lxml")
+    list = soup.select("#waterfall .item")
+    json = {}
+    listnum = len(list)
+    json['total'] = total+listnum
+    for i in range(len(list)):
+        if i>2:
+            break
+        vmsg = {}
+        id = list[i].select("date")[0].text
+        title = list[i].select(".photo-frame >img")[0].get("title").strip()
+        vmsg['summary'] = title
+        title = id + " "+ title
+        vmsg['url'] = list[i].select(".movie-box")[0].get("href").replace(host,"")
+        poster = list[i].select(".photo-frame > img")[0].get("src")
+        # if poster.find("http")==-1:
+        #     poster="https:"+poster
+        vmsg['id'] = id
+        
+        vmsg['sub_title'] = id
+        #检查是否存在字幕
+        if list[i].select(".photo-info"):
+            if list[i].select(".photo-info")[0].select(".btn-warning"):
+                vmsg['caption']=1
+                title = "[字幕]"+title
+            else:
+                vmsg['caption']=0
+        vmsg['title'] = title
+        vmsg['subtype'] = 'movie'
+        vmsg['lang'] = 'jpn'
+        vmsg['type']=1
+        vmsg['poster'] = poster
+        data.append(vmsg)
+    json['data'] = data
+    return json
+
+def main(argv):
+    title = "".join(argv)
+    list =  javlist(title)
+    data = json.dumps(list, ensure_ascii=False)
+    print(data)
+if __name__ == '__main__':
+    main(sys.argv[1:])
